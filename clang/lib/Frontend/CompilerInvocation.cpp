8848d37aa14dc997f47a1016f6d5a752d66e8aa9
Dan Sarginson <dan.sarginson@gmail.com>
2019-09-21 13:44:08 +0100

    Admin: introduce an -fpattern-matching option.
    Defaults to off, passed from driver to cc1

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -2727,6 +2727,8 @@ static void ParseLangArgs(LangOptions &Opts, ArgList &Args, InputKind IK,
                    OPT_fno_double_square_bracket_attributes,
                    Opts.DoubleSquareBracketAttributes);
 
+  Opts.PatternMatching = Args.hasArg(options::OPT_fpattern_matching);
+
   Opts.CPlusPlusModules = Opts.CPlusPlus2a;
   Opts.ModulesTS = Args.hasArg(OPT_fmodules_ts);
   Opts.Modules =
